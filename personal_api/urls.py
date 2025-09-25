4d4459af3914719839ef0795fb76261d0ae8b00a
vibbiz <ramo_basket@live.se>
2025-09-25 09:10:43 +0200

    Update urls.py

--- personal_api/urls.py
+++ personal_api/urls.py
@@ -1,1 +1,5 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+urlpatterns =
