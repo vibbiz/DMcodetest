f7642a72cda2815b34b49f3b9fda1c9b10a82d22
vibbiz <ramo_basket@live.se>
2025-09-25 09:11:18 +0200

    Update serializers.py

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from.models import Employee
 
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ['id', 'first_name', 'last_name', 'email', 'date_joined']
+        read_only_fields = ['id', 'date_joined']
