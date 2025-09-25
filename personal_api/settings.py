f8e5ec0b3cbabefbfb514f865d4fb988a37f71b9
vibbiz <ramo_basket@live.se>
2025-09-25 10:50:04 +0200

    Update settings.py

--- personal_api/settings.py
+++ personal_api/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS =
+ALLOWED_HOSTS = ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Lägg till de appar vi använder
 INSTALLED_APPS = [
